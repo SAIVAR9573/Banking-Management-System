e9b7373dbf9d005965cb0e101a22a82e2a6a0de4
Parepally Saivarshith <YOUR_GITHUB_EMAIL>
2026-08-13 22:26:15 +0530

    Initial commit - Banking Management System

---
+++ src/banking/Conn.java
@@ -0,0 +1,22 @@
+package banking;
+
+
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Your-Mysql-Password");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
